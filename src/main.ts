dc64f0687a56dff347b55c33bcd287dbb6e1fb7f
Maxim <ivannikov.maxim1@gmail.com>
2023-12-27 16:05:21 +0100

    added entity, dto, service, middleware and validation

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { UserController } from './users/users.controller';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { UserService } from './users/users.service';
+import { IUserService } from './users/users.service.interface';
+import { IUserController } from './users/users.controller.interface';
 
 // async function bootstrap() {
 // const logger = new LoggerService();
@@ -19,7 +22,8 @@ export interface IBootstrapReturn {
 export const appBinding = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
 	bind<App>(TYPES.Application).to(App);
 });
 
